026c1c9df76931a91c1c85a2051e6976e2c81f04
Eric <emtordo@gmail.com>
2020-05-08 17:25:08 -0500

    update: docstrings and comments

--- transfermarkt_web_scraper.py
+++ transfermarkt_web_scraper.py
@@ -7,6 +7,16 @@ import pandas as pd
 
 
 def get_clubs_and_transfers(league_name, league_id, season_id, window):
+    """Requests the Transfermarkt page for the input league season and scrapes the page HTML for transfer data.
+
+    Args:
+        league_name (str): Name of the league.
+        league_id (str): League's unique Transfermarkt ID.
+        season_id (str): First calendar year of the season, e.g. '2018' for 2018-19.
+        window (str): 's' for summer or 'w' for winter transfer windows.
+    Returns:
+        A list of the clubs in the league, and two lists of tables (list of lists) for each club's transfer activity. 
+    """
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
     url = "https://www.transfermarkt.co.uk/{league_name}/transfers/wettbewerb/{league_id}/plus/?saison_id={season_id}&s_w={window}".format(league_name=league_name, league_id=league_id, season_id=season_id, window=window)
     try:
@@ -34,6 +44,17 @@ def get_clubs_and_transfers(league_name, league_id, season_id, window):
 
 
 def get_transfer_info(table, movement, season, window, league):
+    """Helper function to parse an HTML table and extract all desired player information.
+
+    Args:
+        table (bs4.element.Tag): BeautifulSoup HTML table.
+        movement (str): 'In' for arrival or 'Out' departure. 
+        season (str): Season.
+        window (str): 's' for summer or 'w' for winter.
+        league (str): League name.
+    Returns:
+        The input table's information reformatted as a list of lists. 
+    """
     transfer_info = []
     trs = table.find_all('tr')
     header_row = [header.get_text(strip=True) for header in trs[0].find_all('th')]
@@ -50,14 +71,18 @@ def get_transfer_info(table, movement, season, window, league):
         for td in tds:
             child = td.findChild()
             if child and child.get('class'):
+                # Player name
                 if child.get('class')[0] == 'di':
                     row.append(child.get_text(strip=True))
+                # Player nationality
                 elif child.get('class')[0] == 'flaggenrahmen':
                     row.append(child.get('alt'))
+                # Club dealt to/from
                 elif child.get('class')[0] == 'vereinprofil_tooltip':
                     row.append(child.findChild().get('alt'))
             else:
                 row.append(td.get_text(strip=True))
+        # Mark tables of no transfer activity with None for later cleaning
         if "No new arrivals" in row or "No departures" in row:
             transfer_info.append([None] * (len(header_row) - 1))
         else:
@@ -68,6 +93,15 @@ def get_transfer_info(table, movement, season, window, league):
 
 
 def formatted_transfers(clubs, transfers_in, transfers_out):
+    """Prepends club names to their transfers.
+
+    Args:
+        clubs (list): List of clubs.
+        transfers_in (list): List of lists.
+        transfers_out (list): List of lists.
+    Return:
+        Updated transfer tables.
+    """
     for i in range(len(clubs)):
         club_name = clubs[i]
         for row in transfers_in[i][1:]:
@@ -79,12 +113,26 @@ def formatted_transfers(clubs, transfers_in, transfers_out):
 
 
 def transfers_dataframe(tables_list):
+    """Converts all transfer tables to dataframes then concatenates them into a single dataframe.
+
+    Args:
+        tables_list (list): List of transfer DataFrames.
+    Returns:
+        A DataFrame of all transfers.
+    """
     return pd.concat([pd.DataFrame(table[1:], columns=table[0]) for table in tables_list])
 
 
 def export_csv(df, season_id, league_name):
+    """Writes an input DataFrame to a csv in its corresponding season's folder.
+
+    Args:
+        df (DataFrame): Transfer data to be exported.
+        season_id (str): Folder in which to write the csv.
+        league_name (str): File name for the csv.
+    """
     file_name = '{}.csv'.format(league_name)
-    path_name = './{}'.format(season_id)
+    path_name = './data/{}'.format(season_id)
     if not os.path.exists(path_name):
         os.mkdir(path_name)
     
@@ -93,6 +141,16 @@ def export_csv(df, season_id, league_name):
 
 
 def scrape_season_transfers(league_name, league_id, season_id, window):
+    """Web scrapes Transfermarkt for all transfer activity in a league's given window.
+
+    Args:
+        league_name (str): Name of the league.
+        league_id (str): League's unique Transfermarkt ID.
+        season_id (str): First calendar year of the season, e.g. '2018' for 2018-19.
+        window (str): 's' for summer or 'w' for winter transfer windows.
+    Returns:
+        A DataFrame of all season transfer activity in the input league.
+    """
     clubs, transfer_in_list, transfer_out_list = get_clubs_and_transfers(league_name, league_id, season_id, window)
     print("Got data for {} {} {} transfer window".format(season_id, league_name.upper(), window.upper()))
     transfers_in, transfers_out = formatted_transfers(clubs, transfer_in_list, transfer_out_list)
@@ -105,10 +163,10 @@ def scrape_season_transfers(league_name, league_id, season_id, window):
 
 
 def main():
-    league_names = ['premier-league']#, 'championship']
-    league_ids = ['GB1']#, 'GB2']
+    league_names = ['premier-league', 'championship']
+    league_ids = ['GB1', 'GB2']
     for league_name, league_id in zip(league_names, league_ids):
-        for i in range(2018, 2019):
+        for i in range(1992, 2019):
             league_transfers = []
             season_id = str(i)
             for window in ['s', 'w']:
